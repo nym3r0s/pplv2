3055405020ee7b69793fa2fd2c5a719b85ff33b1
sarwesh <sarwesh.krshnn@gmail.com>
2015-02-07 23:50:52 +0530

    scores for teams completed

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = 'stein238';
+$dbpass = '';
 $dbname = 'ppl';
 mysql_connect($dbhost,$dbuser,$dbpass) or die('MySQL Connection Failed');
 mysql_select_db($dbname);
